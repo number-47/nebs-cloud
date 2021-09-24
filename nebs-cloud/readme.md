409225353188740ef3c6d4c076b309a2cadc6686
number-47 <820368938@qq.com>
2021-09-24 11:15:47 +0800

    finally

--- nebs-cloud/readme.md
+++ nebs-cloud/readme.md
@@ -1,1 +1,6 @@
 整个项目的父模块
+test
+123
+test-master
+
+test
---
+++ nebs-spring-gateway/src/test/java/com/number47/nebs/gateway/NebsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.number47.nebs.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
