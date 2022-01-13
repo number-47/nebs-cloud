b5fc1966429dad154043bcb75096c195358afd4d
number-47 <820368938@qq.com>
2022-01-13 15:23:24 +0800

    feature:增加docker-compose

---
+++ nebs-cloud/docker/nebs/nebs-cloud/build-image.sh
@@ -0,0 +1,7 @@
+docker build -t nebs-auth /nebs/nebs-auth/
+docker build -t nebs-register /nebs/nebs-register/
+docker build -t nebs-monitor-admin /nebs/nebs-monitor-admin/
+docker build -t nebs-server-test /nebs/nebs-server-test/
+docker build -t nebs-server-system /nebs/nebs-server-system/
+docker build -t nebs-spring-gateway /nebs/nebs-spring-gateway/
+docker build -t zipkin-server /nebs/zipkin-server/
